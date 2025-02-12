829104a418b60bf6f668058750db86e831d9c278
SecretsZhong <383426620@qq.com>
2025-02-12 11:38:45 +0800

    Update ManifestProcessor.cs

--- Editor/ManifestProcessor.cs
+++ Editor/ManifestProcessor.cs
@@ -24,9 +24,6 @@ namespace Bridge.FacebookApi
         private const string METADATA_CLIENT_TOKEN = "com.facebook.sdk.ClientToken";
         private const string FACEBOOK_ACTIVETY_PACKAGE = "com.facebook.FacebookActivity";
         private const string FACEBOOK_CUSTOM_TAB_PACKAGE = "com.facebook.CustomTabActivity";
-        private const string ACTION_VIEW = "android.intent.action.VIEW";
-        private const string DEFAULT_CATEGORY = "android.intent.category.DEFAULT";
-        private const string BROWSABLE_CATEGORY = "android.intent.category.BROWSABLE";
 
         [PostProcessBuild(10001)]
         public static void OnPostprocessBuild(BuildTarget target, string projectPath)
@@ -35,35 +32,18 @@ namespace Bridge.FacebookApi
             string app_id = settings.FbAppId;
             string client_token = settings.FbClientToken;
             string display_name = settings.FbDisplayName;
-            SetFacebookConfig(projectPath, app_id, client_token, display_name);
+            SetFacebookConfig(app_id, client_token, display_name);
             RefreshManifest(app_id);
             CopyNativeCode(projectPath);
             Common.ManifestProcessor.ReplaceBuildDefinedCache[Common.ManifestProcessor.FACEBOOK_DEPENDENCIES] = "implementation(\"com.facebook.android:facebook-android-sdk:17.0.1\")";
         }
 
-        private static void SetFacebookConfig(string projectPath, string app_id, string client_token, string display_name)
+        private static void SetFacebookConfig(string app_id, string client_token, string display_name)
         {
-            string stringsPath = Path.Combine(projectPath, Common.ManifestProcessor.STRINGS_XML_PATH);
-            if (!File.Exists(stringsPath))
-            {
-                Directory.CreateDirectory(stringsPath.Replace("/strings.xml", ""));
-                File.WriteAllText(stringsPath, @"<?xml version=""1.0"" encoding=""utf-8""?>
-<resources>
-</resources>");
-            }
-            XDocument strings = XDocument.Load(stringsPath);
-            XElement resources = strings.Element("resources");
-            if (resources == null)
-            {
-                resources = new XElement("resources");
-                strings.Add(resources);
-            }
-
-            resources.Add(new XElement("string", new XAttribute("name", "facebook_app_id"), app_id));
-            resources.Add(new XElement("string", new XAttribute("name", "fb_login_protocol_scheme"), $"fb{app_id}"));
-            resources.Add(new XElement("string", new XAttribute("name", "facebook_client_token"), client_token));
-            resources.Add(new XElement("string", new XAttribute("name", "display_name"), display_name));
-            resources.Save(stringsPath);
+            Common.ManifestProcessor.StringsElements.Add(new XElement("string", new XAttribute("name", "facebook_app_id"), app_id));
+            Common.ManifestProcessor.StringsElements.Add(new XElement("string", new XAttribute("name", "fb_login_protocol_scheme"), $"fb{app_id}"));
+            Common.ManifestProcessor.StringsElements.Add(new XElement("string", new XAttribute("name", "facebook_client_token"), client_token));
+            Common.ManifestProcessor.StringsElements.Add(new XElement("string", new XAttribute("name", "display_name"), display_name));
         }
 
         private static void RefreshManifest(string app_id)
@@ -86,9 +66,9 @@ namespace Bridge.FacebookApi
                     new XAttribute(Common.ManifestProcessor.ns + "name", FACEBOOK_CUSTOM_TAB_PACKAGE),
                     new XAttribute(Common.ManifestProcessor.ns + "exported", "true"),
                     new XElement("intent-filter",
-                            new XElement("action", new XAttribute(Common.ManifestProcessor.ns + "name", ACTION_VIEW)),
-                            new XElement("category", new XAttribute(Common.ManifestProcessor.ns + "name", DEFAULT_CATEGORY)),
-                            new XElement("category", new XAttribute(Common.ManifestProcessor.ns + "name", BROWSABLE_CATEGORY)),
+                            new XElement("action", new XAttribute(Common.ManifestProcessor.ns + "name", Common.ManifestProcessor.ACTION_VIEW)),
+                            new XElement("category", new XAttribute(Common.ManifestProcessor.ns + "name", Common.ManifestProcessor.DEFAULT_CATEGORY)),
+                            new XElement("category", new XAttribute(Common.ManifestProcessor.ns + "name", Common.ManifestProcessor.BROWSABLE_CATEGORY)),
                             new XElement("data", new XAttribute(Common.ManifestProcessor.ns + "scheme", "@string/fb_login_protocol_scheme")))
                     ));
         }
